592d1b32efa8b11c9acad2bd7ebd4d83b8799b08
Manjari <lk659879@dal.ca>
2020-04-02 22:03:56 -0300

    changed github webhook of CodeBuild to only profile commits

--- microservices/login-signup/src/index.js
+++ microservices/login-signup/src/index.js
@@ -15,7 +15,7 @@ app.listen(process.env.PORT, () =>
 
 // GET API for landing page
 app.get("/profile", (req, res) => {
-  res.send("Hello world - Profile!");
+  res.send("Hello world - Profile ser!");
 });
 
  // POST api to save user to db
